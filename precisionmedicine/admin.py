56e17b81a2896ac6fbcc5311283c2c6a18485af7
rakinplaban <rakinshahriar54@gmail.com>
2022-06-24 20:29:59 +0600

    Made login form

--- precisionmedicine/admin.py
+++ precisionmedicine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from django.contrib.admin.options import ModelAdmin
 # Register your models here.
+
+def User_display(ModelAdmin):
+    list_display = ("id","username","password")
--- precisionmedicine/views.py
+++ precisionmedicine/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
-    return render(request,"precisionmedicine/index.html")
+    if request.user.is_authenticated:
+        return render(request,"precisionmedicine/index.html")
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+
+        else:
+            return render(request,'precisionmedicine/login.html',{
+                "message" : "Sorry, the username or password may be invalid!"
+            })
+
+    else:
+        return render(request,"precisionmedicine/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+def register(request):
+    pass
